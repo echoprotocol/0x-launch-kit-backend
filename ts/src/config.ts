7e9659c781db56940be879d115a44fcf3c2e0719
Nick Borsuk <n.borsuk@pixelplex.io>
2019-11-22 18:04:54 +0300

    migrate to echo

--- ts/src/config.ts
+++ ts/src/config.ts
@@ -17,10 +17,7 @@ enum EnvVarType {
 }
 // Whitelisted token addresses. Set to a '*' instead of an array to allow all tokens.
 export const WHITELISTED_TOKENS: string[] | '*' = _.isEmpty(process.env.WHITELIST_ALL_TOKENS)
-    ? [
-          '0x2002d3812f58e35f0ea1ffbf80a75a38c32175fa', // ZRX on Kovan
-          '0xd0a1e359811322d97991e03f863a0c30c2cf029c', // WETH on Kovan
-      ]
+    ? '*'
     : assertEnvVarType('WHITELIST_ALL_TOKENS', process.env.WHITELIST_ALL_TOKENS, EnvVarType.WhitelistAllTokens);
 
 // Network port to listen on
